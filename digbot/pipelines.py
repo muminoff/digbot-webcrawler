559f6403512854c5d93dae448ae9e3d11031db1e
muminoff <smuminov@gmail.com>
2014-12-07 18:52:33 +0900

    Items  and  fields

--- digbot/pipelines.py
+++ digbot/pipelines.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 
 
 class DigbotPipeline(object):
     def process_item(self, item, spider):
+        item["last_crawled"] = datetime.utcnow()
+        item["spider"] = spider.name
         return item
