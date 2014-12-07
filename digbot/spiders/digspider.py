773eeb71fdb34de9bb1da5d1b4a3fd281cfefa2b
muminoff <smuminov@gmail.com>
2014-12-07 16:15:55 +0900

    Domain whitelist

--- digbot/spiders/digspider.py
+++ digbot/spiders/digspider.py
@@ -4,7 +4,6 @@ from scrapy.utils.markup import remove_tags, remove_tags_with_content
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 import scrapy
 import tldextract
-import html2text
 import redis
 from digbot.items import PageItem
 
@@ -17,6 +16,11 @@ class DigSpider(RedisSpider):
     def is_domestic(self, url):
         return tldextract.extract(url).tld == 'uz'
 
+    def is_domain_in_white_list(self, url):
+        r = redis.Redis(connection_pool=self.pool)
+        domain = tldextract.extract(url).registered_domain
+        return r.sismember('domain_whitelist', domain)
+
     def parse(self, response):
         hxs = scrapy.Selector(response)
         refer_links = self.link_extractor.extract_links(response)
@@ -27,12 +31,15 @@ class DigSpider(RedisSpider):
             item['title'] = hxs.xpath('/html/head/title/text()').extract()[0]
         except:
             item['title'] = tldextract.extract(response.url).registered_domain
-        # item['content'] = html2text.html2text(response.body.decode('utf8'))
         yield item
 
         r = redis.Redis(connection_pool=self.pool)
 
         for link in tld_links:
             if not r.sismember('visited_urls', link):
-                r.sadd('visited_urls', link)
-                yield scrapy.http.Request(url=link, callback=self.parse)
+                if self.is_domain_in_white_list(link):
+                    scrapy.log.msg('Following link {}'.format(link))
+                    r.sadd('visited_urls', link)
+                    yield scrapy.http.Request(url=link, callback=self.parse)
+                else:
+                    scrapy.log.msg('Domain not in white list {}'.format(link))
