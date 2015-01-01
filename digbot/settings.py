ea55202d7d57b370281828f87088bc255f0c3fb1
muminoff <smuminov@gmail.com>
2015-01-01 16:32:30 +0900

    For testing

--- digbot/settings.py
+++ digbot/settings.py
@@ -6,7 +6,7 @@ SPIDER_MODULES = ['digbot.spiders']
 NEWSPIDER_MODULE = 'digbot.spiders'
 
 USER_AGENT = 'DigBot'
-LOG_LEVEL = 'CRITICAL'
+LOG_LEVEL = 'DEBUG'
 # LOG_FILE = './log/digspider.log'
 
 # Broad crawler settings recommendation from scrapy authors
