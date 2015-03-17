96fed3ec80a9b9733ecb2ff307dea209cf18eef5
Sardor <sardor@hanbiro.com>
2015-03-17 17:54:44 +0900

    DB index

--- digbot/models.py
+++ digbot/models.py
@@ -19,19 +19,19 @@ def create_pages_table(engine):
 class Page(DeclarativeBase):
     __tablename__ = "pages"
 
-    id = Column(BigInteger, primary_key=True)
+    id = Column(BigInteger, primary_key=True, index=True)
     url = Column('url', String, nullable=False)
     title = Column('title', String, nullable=False)
     content = Column('content', UnicodeText)
-    spider = Column('spider', String)
-    last_crawled = Column('last_crawled', DateTime)
+    spider = Column('spider', String, index=True)
+    last_crawled = Column('last_crawled', DateTime, index=True)
 
 
 class Domain(DeclarativeBase):
     __tablename__ = "domains"
 
-    fqdn = Column('title', String, primary_key=True)
-    ip_address = Column('ip_address', postgresql.INET, nullable=False)
-    tasix_member = Column('tasix_member', Boolean, unique=False, default=False)
-    last_crawled = Column('last_crawled', DateTime)
+    fqdn = Column('title', String, primary_key=True, index=True)
+    ip_address = Column('ip_address', postgresql.INET, nullable=False, index=True)
+    tasix_member = Column('tasix_member', Boolean, unique=False, default=False, index=True)
+    last_crawled = Column('last_crawled', DateTime, index=True)
     spider = Column('spider', String)
